bc52a199324726d3faa08c559f30c2f5a5399111
Joham Medina <joham.nba@gmail.com>
2020-08-07 17:42:35 -0500

    Advance AlgortimosClasificacion-Credit-Card-Approval

--- R/ScriptR-CreditCardApproval.R
+++ R/ScriptR-CreditCardApproval.R
@@ -12,21 +12,45 @@ library(readr)
 library(dplyr)
 library(magrittr)
 
-application_record <- read_csv(".../application_record.csv")
-credit_record <- read_csv(".../credit_record.csv")
-credit_record$MONTHS_BALANCE <- NULL
+# Cargar dataSets application_record
+application<- read_csv("dataSets/application_record.csv")
+cat("Numero Filas y Columnas dataSet application: ",dim(application))
 
-credit_record
+print("Valores nulos en dataSet application :")
+sapply(application, function(x) sum(is.na(x)))
 
-credit <- credit_record %>% 
-          group_by(ID) %>% 
-          summarise(STATUS = list(STATUS))
+# Eliminar filas con valor NaN en atributo OCCUPATION_TYPE
+application <- application[!is.na(application$OCCUPATION_TYPE),]
+# datos <- na.omit(datos) # Eliminar todos las filas con valor NaN
 
-credit2 <- credit_record %>% 
+cat("Nuevo mumero Filas y Columnas dataSet application: ",dim(application))
+
+# -----------------------------------------------------------------------------
+
+# Cargar dataSets credit_record
+credit <- read_csv("dataSets/credit_record.csv")
+cat("Numero Filas y Columnas dataSet credit: ",dim(credit))
+
+print("Valores nulos en dataSet credit :")
+sapply(credit, function(x) sum(is.na(x)))
+
+# Eliminar atributo(Columna) MONTHS_BALANCE en dataSet credit
+credit$MONTHS_BALANCE <- NULL
+
+cat("Nuevo mumero Filas y Columnas dataSet application: ",dim(credit))
+
+# credit <- credit_record %>% 
+#           group_by(ID) %>% 
+#           summarise(STATUS = list(STATUS))
+
+credit <- credit %>% 
   group_by(ID) %>% 
   summarise(STATUS = paste0(STATUS,collapse = ""))
 
+# -----------------------------------------------------------------------------
 
-dataSet <- merge(x = application_record, y = credit_record, by = "ID")
-
+# Unir dataSets application_record y credit_record
+dataSet <- merge(x = application, y = credit, by = "ID")
+cat("Numero Filas y Columnas dataSet: ",dim(dataSet))
 dataSet <- na.omit(dataSet)   # Eliminar filas con valores faltantes
+cat("Nuevo mumero Filas y Columnas dataSet: ",dim(dataSet))
